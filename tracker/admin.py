1d70ccf2fd6d44fd9e9fc19eee231c27382a7aab
osedunme quincy <quincyosedunme@gmail.com>
2026-08-06 22:58:42 +0100

    Register models in Django admin, verify constraints work

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Technology, HypeSnapshot, DemandSnapshot, ScrapeLog
 
-# Register your models here.
+admin.site.register(Technology)
+admin.site.register(HypeSnapshot)
+admin.site.register(DemandSnapshot)
+admin.site.register(ScrapeLog)
