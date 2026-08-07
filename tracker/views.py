b9368b918d137ecf29edc5cfd5336749e066ce1a
osedunme quincy <quincyosedunme@gmail.com>
2026-08-07 12:56:59 +0100

    Add TechnologyHistoryView combining hype and demand snapshots

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
-from .models import Technology
-from .serializers import TechnologySerializer
+from .models import Technology, HypeSnapshot, DemandSnapshot
+from .serializers import TechnologySerializer, HypeSnapshotSerializer, DemandSnapshotSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class TechnologyListView(generics.ListAPIView):
@@ -10,3 +12,18 @@ class TechnologyListView(generics.ListAPIView):
 class TechnologyDetailView(generics.RetrieveAPIView):
     queryset = Technology.objects.all()
     serializer_class = TechnologySerializer
+
+class TechnologyHistoryView(APIView):
+    def get(self, request, pk):
+        technology = Technology.objects.get(pk=pk)
+        hype_snapshots = HypeSnapshot.objects.filter(technology=technology)
+        demand_snapshots = DemandSnapshot.objects.filter(technology=technology)
+
+        hype_data = HypeSnapshotSerializer(hype_snapshots, many=True).data
+        demand_data = DemandSnapshotSerializer(demand_snapshots, many=True).data
+
+        return Response({
+            'technology': technology.name,
+            'hype_history': hype_data,
+            'demand_history': demand_data,
+        })
